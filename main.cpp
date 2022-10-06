b970c1a38c343b1c03b1447cbc16076582d4908c
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-06 11:00:33 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: Anita Prova
+ * Course: CSCI-135
+ * Instructor: Genady Maryash
+ * Assignment: Homework E5.14
+ * The program swaps two integers.
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sort3(int& a, int& b, int& c){
+
+}
